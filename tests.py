b3efc33e41713fc14b2992ae0774cd24bda9ce0e
Mihir Singh <me@mihirsingh.com>
2013-03-06 20:28:31 -0500

    Updated for v0.1.4.

--- tests.py
+++ tests.py
@@ -1,36 +1,39 @@
-import os
 import json
 import unittest
 from pyunio import pyunio
 
-pyunio = pyunio.PyUnio(os.getcwd())
-
 pyunio.use('httpbin')
 
 params = {
-	'name': 'James Bond'
-}
+            'body': {
+                        'name': 'James Bond'
+                    }
+         }
+
 
 def test_get():
 
-	response = json.loads(pyunio.get('get', params).text)
+    response = json.loads(pyunio.get('get', params).text)
 
-	assert(response['args']['name'] == 'James Bond')
+    assert(response['args']['name'] == 'James Bond')
+
 
 def test_post():
 
-	response = json.loads(pyunio.post('post', params).text)
+    response = json.loads(pyunio.post('post', params).text)
 
-	assert(response['args']['name'] == 'James Bond')
+    assert(response['args']['name'] == 'James Bond')
+
 
 def test_put():
 
-	response = json.loads(pyunio.put('put', params).text)
+    response = json.loads(pyunio.put('put', params).text)
 
-	assert(response['args']['name'] == 'James Bond')
+    assert(response['args']['name'] == 'James Bond')
+
 
 def test_delete():
 
-	response = json.loads(pyunio.delete('delete', params).text)
+    response = json.loads(pyunio.delete('delete', params).text)
 
-	assert(response['args']['name'] == 'James Bond')
+    assert(response['args']['name'] == 'James Bond')
