044460c5b0377d920ae204cd94cb4daea3205915
Mihir Singh <me@mihirsingh.com>
2013-03-06 20:28:15 -0500

    Easier to use.

---
+++ pyunio/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'pyunio'
+__version__ = '0.1.4'
+__author__ = 'Mihir Singh (citruspi)'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Mihir Singh <me@mihirsingh.com>'
+
+from .pyunio import pyunio
