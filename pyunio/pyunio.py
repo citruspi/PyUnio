a826a5c9249c95a31b610d3cab6ddf3fd5aa32c8
Mihir Singh <me@mihirsingh.com>
2013-03-06 20:28:05 -0500

    Cleaned up and implemented headers.

--- pyunio/pyunio.py
+++ pyunio/pyunio.py
@@ -1,163 +1,184 @@
-##############################################
-# Name        : PyUnio
-# Author      : Mihir Singh (@citruspi)
-# Version     : 0.1.3
-# License     : MIT
-# Description : Basic Python implementation
-#			    of @ttezel's Node.js unio.
-##############################################
+__title__ = 'PyUnio'
+__version__ = '1.1.0'
+__author__ = 'Mihir Singh (citruspi)'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2013 Mihir Singh'
 
 try:
 
-	import os
-	import json
-	import requests
-	from inspect import stack
-	from requests_oauthlib import OAuth1
-	
+    import json
+    import requests
+    from inspect import stack
+    from requests_oauthlib import OAuth1
+
 except ImportError as e:
-	
-	raise Exception(e)
 
-class PyUnio(object):
-	
-	def __init__(self, cwd):
-		
-		self.specs = dict	
-		self.cwd   = cwd 
-				
-	def use(self, service):
-				
-		try:
-	
-			with open(self.cwd+'/specs/'+service+'.json') as service_definition: self.specs = json.load(service_definition)
-													
-		except Exception as e:
-			
-			raise Exception(e)
-			
-	def get(self, route, params):
+    raise Exception(e)
 
-		try:		
-	
-			if stack()[0][3] in self.specs['resources'][route]['methods']:
-				
-				if ('oauth') in params:
-					
-						oauth, params = self.oauth_handle(params)
-															   
-						return getattr(requests, stack()[0][3])(self.specs['api_root']+'/'+self.specs['resources'][route]['path'],
-																 params=params,
-												 				 auth=oauth)														
-					
-				else:
-					
-					return getattr(requests, stack()[0][3])(self.specs['api_root']+'/'+self.specs['resources'][route]['path'],
-														 params=params)
-	
-			else:
-	
-				raise Exception("The HTTP verb '%s' is not defined for '%s'.") % (stack()[0][3], route)	
-	
-		except KeyError as e:
-	
-			raise Exception(e)												
-				
-	def post(self, route, params):
-		
-		try:		
-		
-			if stack()[0][3] in self.specs['resources'][route]['methods']:
-				
-				if ('oauth') in params:
-					
-						oauth, params = self.oauth_handle(params)
-															   
-						return getattr(requests, stack()[0][3])(self.specs['api_root']+'/'+self.specs['resources'][route]['path'],
-																 params=params,
-												 				 auth=oauth)															
-					
-				else:
-					
-					return getattr(requests, stack()[0][3])(self.specs['api_root']+'/'+self.specs['resources'][route]['path'],
-														 params=params)
-		
-			else:
-		
-				raise Exception("The HTTP verb '%s' is not defined for '%s'.") % (stack()[0][3], route)	
-		
-		except KeyError as e:
-		
-			raise Exception(e)	
-		
-	def put(self, route, params):
-		
-		try:		
-		
-			if stack()[0][3] in self.specs['resources'][route]['methods']:
-				
-				if ('oauth') in params:
-					
-						oauth, params = self.oauth_handle(params)
-															   
-						return getattr(requests, stack()[0][3])(self.specs['api_root']+'/'+self.specs['resources'][route]['path'],
-																 params=params,
-												 				 auth=oauth)														
-					
-				else:
-					
-					return getattr(requests, stack()[0][3])(self.specs['api_root']+'/'+self.specs['resources'][route]['path'],
-														 params=params)
-		
-			else:
-		
-				raise Exception("The HTTP verb '%s' is not defined for '%s'.") % (stack()[0][3], route)	
-		
-		except KeyError as e:
-		
-			raise Exception(e)		
-		
-	def delete(self, route, params):
-		
-		try:		
-		
-			if stack()[0][3] in self.specs['resources'][route]['methods']:
-				
-				if ('oauth') in params:
-					
-						oauth, params = self.oauth_handle(params)
-															   
-						return getattr(requests, stack()[0][3])(self.specs['api_root']+'/'+self.specs['resources'][route]['path'],
-																 params=params,
-												 				 auth=oauth)															
-					
-				else:
-					
-					return getattr(requests, stack()[0][3])(self.specs['api_root']+'/'+self.specs['resources'][route]['path'],
-														 params=params)
-		
-			else:
-		
-				raise Exception("The HTTP verb '%s' is not defined for '%s'.") % (stack()[0][3], route)	
-		
-		except KeyError as e:
-		
-			raise Exception(e)			
-			
-	def oauth_handle(self, params):
-		
-		try:
-	
-			oauth = OAuth1(params['ouath']['consumer_key'],       		
-    					   params['ouath']['consumer_secret'],
-                           params['ouath']['access_token'], 
+
+class pyunio(object):
+
+    def __init__(self):
+
+        self.specs = dict
+
+    def use(self, service):
+
+        try:
+
+            with open('specs/' + service + '.json') as service_definition:
+                self.specs = json.load(service_definition)
+
+        except Exception as e:
+
+            raise Exception(e)
+
+    def get(self, route, params):
+
+        if 'body' not in params: params['body'] = {}
+        if 'header' not in params: params['header'] = {}
+
+        try:
+
+            if stack()[0][3] in self.specs['resources'][route]['methods']:
+
+                if 'oauth' in params:
+
+                    return getattr(
+                        requests, stack()[0][3])(self.specs['api_root'] + '/' + self.specs['resources'][route]['path'],
+                                                 params=params['body'],
+                                                 headers=params['header'],
+                                                 auth=self.oauth_handle(params))
+
+                else:
+
+                    return getattr(
+                        requests, stack()[0][3])(self.specs['api_root'] + '/' + self.specs['resources'][route]['path'],
+                                                 params=params['body'],
+                                                 headers=params['header'])
+
+            else:
+
+                raise Exception("The HTTP verb '%s' is not defined for '%s'.") % (
+                    stack()[0][3], route)
+
+        except KeyError as e:
+
+            raise Exception(e)
+
+    def post(self, route, params):
+
+        if 'body' not in params: params['body'] = {}
+        if 'header' not in params: params['header'] = {}
+
+        try:
+
+            if stack()[0][3] in self.specs['resources'][route]['methods']:
+
+                if 'oauth' in params:
+
+                    return getattr(
+                        requests, stack()[0][3])(self.specs['api_root'] + '/' + self.specs['resources'][route]['path'],
+                                                 params=params['body'],
+                                                 headers=params['header'],
+                                                 auth=self.oauth_handle(params))
+
+                else:
+
+                    return getattr(
+                        requests, stack()[0][3])(self.specs['api_root'] + '/' + self.specs['resources'][route]['path'],
+                                                 params=params['body'],
+                                                 headers=params['header'])
+
+            else:
+
+                raise Exception("The HTTP verb '%s' is not defined for '%s'.") % (
+                    stack()[0][3], route)
+
+        except KeyError as e:
+
+            raise Exception(e)
+
+    def put(self, route, params):
+
+        if 'body' not in params: params['body'] = {}
+        if 'header' not in params: params['header'] = {}
+
+        try:
+
+            if stack()[0][3] in self.specs['resources'][route]['methods']:
+
+                if ('oauth') in params:
+
+                    return getattr(
+                        requests, stack()[0][3])(self.specs['api_root'] + '/' + self.specs['resources'][route]['path'],
+                                                 params=params['body'],
+                                                 headers=params['header'],
+                                                 auth=self.oauth_handle(params))
+
+                else:
+
+                    return getattr(
+                        requests, stack()[0][3])(self.specs['api_root'] + '/' + self.specs['resources'][route]['path'],
+                                                 params=params['body'],
+                                                 headers=params['header'])
+
+            else:
+
+                raise Exception("The HTTP verb '%s' is not defined for '%s'.") % (
+                    stack()[0][3], route)
+
+        except KeyError as e:
+
+            raise Exception(e)
+
+    def delete(self, route, params):
+
+        if 'body' not in params: params['body'] = {}
+        if 'header' not in params: params['header'] = {}
+
+        try:
+
+            if stack()[0][3] in self.specs['resources'][route]['methods']:
+
+                if ('oauth') in params:
+
+                    return getattr(
+                        requests, stack()[0][3])(self.specs['api_root'] + '/' + self.specs['resources'][route]['path'],
+                                                 params=params['body'],
+                                                 headers=params['header'],
+                                                 auth=self.oauth_handle(params))
+
+                else:
+
+                    return getattr(
+                        requests, stack()[0][3])(self.specs['api_root'] + '/' + self.specs['resources'][route]['path'],
+                                                 params=params['body'],
+                                                 headers=params['header'])
+
+            else:
+
+                raise Exception("The HTTP verb '%s' is not defined for '%s'.") % (
+                    stack()[0][3], route)
+
+        except KeyError as e:
+
+            raise Exception(e)
+
+    def oauth_handle(self, params):
+
+        try:
+
+            oauth = OAuth1(params['ouath']['consumer_key'],
+                           params['ouath']['consumer_secret'],
+                           params['ouath']['access_token'],
                            params['ouath']['access_token_secret'],
                            signature_type='auth_header')
-								   
-			del params['ouath']
-					
-			return (oauth, params)
-			
-		except Exception as e:
-			
-			raise Exception(e)											
+
+            return (oauth)
+
+        except Exception as e:
+
+            raise Exception(e)
+
+pyunio = pyunio()
