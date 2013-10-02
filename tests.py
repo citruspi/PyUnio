a8ccc99aa0923be9a102bb6d42590d3214d4d229
charles <charles@chhsu.com>
2013-10-02 13:27:27 +0800

    Add pyuniotest class for unittest, but still lake mock server for test.

--- tests.py
+++ tests.py
@@ -10,30 +10,27 @@ params = {
                     }
          }
 
+class pyuniotTest(unittest.TestCase): 
 
-def test_get():
-
-    response = json.loads(pyunio.get('get', params).text)
-
-    assert(response['args']['name'] == 'James Bond')
-
-
-def test_post():
-
-    response = json.loads(pyunio.post('post', params).text)
-
-    assert(response['args']['name'] == 'James Bond')
+    def test_get(self):
+        response = json.loads(pyunio.get('get', params).text)
+        self.assertEqual(response['args']['name'], 'James Bond')
 
 
-def test_put():
-
-    response = json.loads(pyunio.put('put', params).text)
-
-    assert(response['args']['name'] == 'James Bond')
+    def test_post(self):
+        response = json.loads(pyunio.post('post', params).text)
+        self.assertEqual(response['args']['name'], 'James Bond')
 
 
-def test_delete():
+    def test_put(self):
+        response = json.loads(pyunio.put('put', params).text)
+        self.assertEqual(response['args']['name'], 'James Bond')
 
-    response = json.loads(pyunio.delete('delete', params).text)
 
-    assert(response['args']['name'] == 'James Bond')
+    def test_delete(self):
+        response = json.loads(pyunio.delete('delete', params).text)
+        self.assertEqual(response['args']['name'], 'James Bond')
+
+
+if __name__ == '__main__':
+    unittest.main()    
